18e12c25d32ef01aba32c65225bdd9cfcf391292
Merve Erdol <merve-uygun@windowslive.com>
2017-12-13 16:47:47 +0300

    Countdown from seconds

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
+let countdown;
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
-    console.log(now, then);
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now())  / 1000);
+        if (secondsLeft <= 0) {
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    console.log(seconds);
 }
